b37529b46c23f1a2417478430cd964602c4e866c
Bruce Korb <bkorb@gnu.org>
2013-10-18 14:32:03 -0700

    replace GNU address with URL

--- lib/liballoca.h
+++ lib/liballoca.h
@@ -12,10 +12,9 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.  */
+   along with this program; if not, see <http://gnu.org/licenses/gpl.html>
 
-/* When this file is included, it may be preceded only by preprocessor
+   When this file is included, it may be preceded only by preprocessor
    declarations.  Thanks to AIX.  Therefore we include it right after
    "config.h", not later.  */
 
