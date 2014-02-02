088230fb83c309fbf1a1c4849c5a08fcf4f33358
Bruce Korb <bkorb@gnu.org>
2014-02-02 11:32:22 -0800

    happy new year

--- src/uudecode.c
+++ src/uudecode.c
@@ -2,23 +2,25 @@
 static char const cright_years_z[] =
 
 /* uudecode utility.
-   Copyright (C) */ "1994-2013";
+   Copyright (C) */ "1994-2014";
 
 /* Free Software Foundation, Inc.
 
-   This product is free software; you can redistribute it and/or modify
+   This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3, or (at your option)
    any later version.
 
-   This product is distributed in the hope that it will be useful,
+   This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, see:  http://www.gnu.org/licenses.
-*/
+   You should have received a copy of the GNU General Public License along
+   with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   --copyright-mark "(copyright \\(c\\)[ \t]+[*]/ *\")([12][90][0-9][0-9])"
+ */
 
 /* Copyright (c) 1983 Regents of the University of California.
    All rights reserved.
